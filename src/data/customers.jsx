799ba9fd78582ac2090993ee4f197a62d32f3e1a
HimaniSolanki1812 <himanisolanki1812@gmail.com>
2026-02-04 16:38:58 +0530

    deleted Some Files

--- src/data/customers.jsx
+++
@@ -1,4 +0,0 @@
-export const customers = [
-  { id: 1, name: "Himani", mobile: "9876543210", address: "Surat", pending: 1500 },
-  { id: 2, name: "Apexa", mobile: "9876501234", address: "Ahmedabad", pending: 0 },
-];
--- src/data/products.jsx
+++
@@ -1,4 +0,0 @@
-export const products = [
-  { id: 1, name: "LED TV", price: 25000, stock: 10 },
-  { id: 2, name: "LCD TV", price: 20000, stock: 5 },
-];
